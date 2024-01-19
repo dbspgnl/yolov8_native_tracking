cd92b8de66dbc5dea6d36ba77a3d71ed4bd81a87
swpark@yellotail.com <swpark@yellotail.com>
2024-01-19 18:08:06 +0900

    * 테스트를 위한 영역 변경 ( 감지 영역 세로 길이 +200 ) * 이미지 오버레이 블렌딩 주석 처리 * 디텍팅 포함된 라벨 영역 하나로 통합하고 디텍팅 중복 제거 * 클래스 미발견시 0으로 처리하는 코드 제거

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ COLORS = sv.ColorPalette.from_hex(['#ff8a0d', '#5d9bff', '#7cae01', '#ffeb00']) 
 
 ZONE_IN_POLYGONS = [
     np.array([
-        [87, 532],[1783, 532],[1783, 612],[87, 612]
+        [87, 532],[1783, 532],[1783, 812],[87, 812]
     ])
 ]
 
@@ -32,23 +32,23 @@ def initiate_polygon_zones(
         for polygon in polygons
     ]
 
-class ImageOverlayBlending:
-    def __init__(self, imgPath) -> None:
-        self.imgPath:str = imgPath
-        self.mix:float = 0
-        cv2.namedWindow("OpenCV View") #
-        cv2.createTrackbar('Mixing', 'OpenCV View', 0,100, lambda x:x) 
+# class ImageOverlayBlending:
+#     def __init__(self, imgPath) -> None:
+#         self.imgPath:str = imgPath
+#         self.mix:float = 0
+#         cv2.namedWindow("OpenCV View") #
+#         cv2.createTrackbar('Mixing', 'OpenCV View', 0,100, lambda x:x) 
     
-    def setMix(
-        self,
-        numpy_array
-    ) -> np.array:
-        numpy_array = cv2.resize(numpy_array, dsize=(1920, 1080), interpolation=cv2.INTER_AREA) #
-        im2 = cv2.imread(self.imgPath) #
-        im2 = cv2.resize(im2, dsize=(1920, 1080), interpolation=cv2.INTER_AREA) #
-        img = cv2.addWeighted(numpy_array, float(100-self.mix)/100, im2 , float(self.mix)/100, 0) #
-        self.mix = cv2.getTrackbarPos('Mixing','OpenCV View')
-        return img
+#     def setMix(
+#         self,
+#         numpy_array
+#     ) -> np.array:
+#         numpy_array = cv2.resize(numpy_array, dsize=(1920, 1080), interpolation=cv2.INTER_AREA) #
+#         im2 = cv2.imread(self.imgPath) #
+#         im2 = cv2.resize(im2, dsize=(1920, 1080), interpolation=cv2.INTER_AREA) #
+#         img = cv2.addWeighted(numpy_array, float(100-self.mix)/100, im2 , float(self.mix)/100, 0) #
+#         self.mix = cv2.getTrackbarPos('Mixing','OpenCV View')
+#         return img
 
 
 class FFmpegProcessor:
@@ -97,6 +97,7 @@ class VideoProcessor:
         self.target_video_path = target_video_path
 
         self.model = YOLO(source_weights_path)
+        self.tracker = sv.ByteTrack()
         
         ffmpeg = FFmpegProcessor(source_video_path, target_video_path)
         self.process = ffmpeg.setPath()
@@ -107,54 +108,58 @@ class VideoProcessor:
         )
         self.bounding_box_annotator = sv.BoundingBoxAnnotator(color=COLORS, thickness=0)
         self.label_annotator = sv.LabelAnnotator(color=COLORS, text_scale=0.35, text_padding=2, color_lookup=sv.ColorLookup.CLASS)
-        self.overlay = ImageOverlayBlending("1920.png") # 블렌딩 이미지 경로
+        # self.overlay = ImageOverlayBlending("1920.png") # 블렌딩 이미지 경로
         self.identity = dict()
         self.frame_number = 0
 
     def process_video(self): 
-        
         for result in self.model.track(source=self.source_video_path, show=False, stream=True, device=0, verbose=False, agnostic_nms=True, imgsz=1920):
             now = datetime.now()
             timestamp = now.timestamp()
             format_time = now.strftime('%Y-%m-%d %H:%M:%S)')
-            self.frame_number += 1
-            # print(f"time: {format_time}  | frame: {self.frame_number} ")
+            self.frame_number += 1 # 현재 프레임 카운팅
             
             if result.boxes.id is None: # 검출이 안되면 스킵
                 continue
             frame = result.orig_img
             detections = sv.Detections.from_ultralytics(result)
-            
-            self.set_identity(detections)
+            # detections.class_id = np.zeros(len(detections))
+            detections = self.tracker.update_with_detections(detections)
             
             # Tracker id
-            detections.tracker_id = result.boxes.id.cpu().numpy().astype(int)
-            # Detect Area 처리
-            detections_in_zones = []
-            for zone_in in self.zones_in:
-                detections_in_zone = detections[zone_in.trigger(detections=detections)]
-                detections_in_zones.append(detections_in_zone)
-            detections = sv.Detections.merge(detections_in_zones)
+            if result.boxes is None or result.boxes.id is None:
+                detections.tracker_id = result.boxes.id.cpu().numpy().astype(int)
 
             # Annotation 처리
             annotated_frame = self.annotate_frame(frame, detections)
             # Output 처리
             numpy_array = np.array(annotated_frame)
             # 오버레이 블렌딩 처리
-            numpy_array = self.overlay.setMix(numpy_array)
+            # numpy_array = self.overlay.setMix(numpy_array)
             
             if numpy_array is not None:
                 self.process.stdin.write(numpy_array.tobytes()) # Output FFmepg
                 cv2.imshow("OpenCV View", numpy_array)
             
-            if (cv2.waitKey(30) == 27): # ESC > stop
+            if (cv2.waitKey(1) == 27): # ESC > stop
                 break
+
             
     def annotate_frame(
         self, frame: np.ndarray, detections: sv.Detections
     ) -> np.ndarray:
         annotated_frame = frame.copy()
-        # 영역 처리
+        
+        self.set_identity(detections)
+        
+        # Detect Area 감지 영역 처리
+        detections_in_zones = []
+        for zone_in in self.zones_in:
+            detections_in_zone = detections[zone_in.trigger(detections=detections)]
+            detections_in_zones.append(detections_in_zone)
+        detections = sv.Detections.merge(detections_in_zones)
+
+        # 영역 테두리 처리
         for i, zone_in in enumerate(self.zones_in):
             annotated_frame = sv.draw_polygon(
                 annotated_frame, zone_in.polygon, sv.Color.from_hex('#ffffff')
@@ -166,18 +171,21 @@ class VideoProcessor:
                 text_thickness=1,
                 text_scale=1
             )
-        # 바운딩 박스
+            
+        # 오브젝트 바운딩 박스
         annotated_frame = self.bounding_box_annotator.annotate(
             scene=annotated_frame, detections=detections
         )
-        # 라벨
+        # 오브젝트 라벨
         labels = [
-            # f"{tracker_id} {self.model.names[class_id]} {confidence:0.2f}"
-            f"{tracker_id} : {self.identity[tracker_id]['speed']}km"
+            f"{tracker_id} {self.model.names[class_id]} {confidence:0.2f}"
+            # f"{tracker_id} : {self.identity[tracker_id]['speed']}km"
+            # f"{self.identity[tracker_id]['id']} : {self.identity[tracker_id]['speed']}km"
             for confidence, class_id, tracker_id
             in zip(detections.confidence, detections.class_id, detections.tracker_id)
         ]
-            
+
+        # 프레임 라벨 처리
         annotated_frame = self.label_annotator.annotate(
             scene=annotated_frame, detections=detections, labels=labels
         )
@@ -188,7 +196,7 @@ class VideoProcessor:
             xyxy = detections.xyxy[i]
             tracker_id = detections.tracker_id[i]
             frame_number = self.frame_number
-            center = (round((xyxy[0]+xyxy[2])/2, 2), round((xyxy[1]+xyxy[3])/2),2)
+            center = (round((xyxy[0]+xyxy[2])/2, 2), round((xyxy[1]+xyxy[3])/2,2))
             if tracker_id not in self.identity:
                 self.identity[tracker_id] = {
                     "id": tracker_id,
@@ -202,6 +210,7 @@ class VideoProcessor:
                 self.identity[tracker_id]['center_array'].append(center)
                 speed = self.estimatespeed(self.identity[tracker_id]['center_array'][-1], self.identity[tracker_id]['center_array'][-2])
                 self.identity[tracker_id]['speed'] = speed
+
     
     def estimatespeed(self, Location1, Location2):
         location_x = abs(Location2[0] - Location1[0])
